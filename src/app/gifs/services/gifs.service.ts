34a059af629ecd912c2a5694a0c42de824f58a59
Gabriel Fabro <fabrogabrielandres@gmail.com>
2022-01-25 13:08:55 -0600

    httpparms

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifResponse } from '../interfaces/interfase';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class GifsService {
+	private _apiKey: string = 'cb12T3ni9T5H1wR1GPZawZTV51JTmm1K';
+	private _historial: string[] = [];
+	private _servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+
+	public resultados: Gif[] = [];
+
+	get historial() {
+		return [ ...this._historial ];
+	}
+
+	constructor(private http: HttpClient) {
+		this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+		this.resultados = JSON.parse(localStorage.getItem('ultimoResultado')!) || [];
+	}
+
+	buscarGifs(query: string) {
+		query = query.trim().toLocaleLowerCase();
+		if (!(query.length === 0 || this._historial.includes(query))) {
+			// es una alternativa this._historial=[query,...this._historial ]
+			this._historial.unshift(query);
+			this._historial = this._historial.splice(0, 10);
+			localStorage.setItem('historial', JSON.stringify(this._historial));
+		}
+
+		const params = new HttpParams().set('api_key', this._apiKey).set('limit', '10').set('q', query);
+
+		this.http.get<SearchGifResponse>(`${this._servicioUrl}/search`, { params: params })
+		  .subscribe((res) => {
+			this.resultados = res.data;
+			localStorage.setItem('ultimoResultado', JSON.stringify(res.data));
+		});
+	}
+}
